a6bbf3d1675057570fd73af9d20accbee861aedb
Carolyn Challoner <16659436+cchalloner@users.noreply.github.com>
2025-10-16 21:23:41 -0500

    Create staging tables for Customer and Product

---
+++ SQL/CreateStage.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dbo.Stg_Customer (
+    CustomerID   VARCHAR(20)  NOT NULL,
+    CustomerName VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE dbo.Stg_Product (
+    ProductID    VARCHAR(20)  NOT NULL,
+    ProductName  VARCHAR(100) NOT NULL
+);
